38ec75300d50089910863eb14731bff28465577a
Adam Wójs <adam@wojs.pl>
2012-11-10 16:16:38 +0100

    Rozwiązanie zadania nr 1: "Hello World"

---
+++ adamwojs/HelloWorld/src/pl/wojs/java/examples/HelloWorld.java
@@ -0,0 +1,22 @@
+/**
+ * Rozwiązanie pierwszego zadania: Hello World 
+ */
+
+package pl.wojs.java.examples;
+
+/**
+ * @author Adam Wójs <adam@wojs.pl>
+ * @package java-examples
+ */
+public class HelloWorld {
+
+	/**
+	 * Entry point
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// Wypusujemy "Hello World!" :-)
+		System.out.println("Hello World!");
+	}
+}
